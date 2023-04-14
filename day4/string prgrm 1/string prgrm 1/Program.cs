35a7a7d3dc8d501a8fad5a2063bd229ef2efced0
janishap <72866270+janishap@users.noreply.github.com>
2023-04-14 13:29:08 +0530

    Add files via upload

---
+++ day4/string prgrm 1/string prgrm 1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//input a string and print that string
+
+using System;
+
+namespace string_prgrm_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string:");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("the string is :" + a);
+
+
+        }
+    }
+}
---
+++ day4/string prgrm 2/string prgrm 2/Program.cs
@@ -0,0 +1,64 @@
+﻿//Write a program in C# Sharp to compare two string without using string library function
+
+using System;
+
+
+namespace string_prgrm_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first string");
+            string x = Console.ReadLine();
+            Console.WriteLine("Enter the second string");
+            string y = Console.ReadLine();
+            int r = 0;
+
+           int a = (x.Length);
+            int b = (y.Length);
+            if (a == b)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    if (x[i] == y[i])
+                    {
+                         r++;
+                    }
+                    else
+                    {
+                       r = 0; 
+                    }
+
+
+                }
+                
+            
+            }
+            else
+            {
+                Console.WriteLine("string  doesn't have  same length");
+            }
+
+
+            if (r==0)
+            {
+                Console.WriteLine("String having same length but not same word");
+            }
+            else
+            {
+                Console.WriteLine("string having same length and  it is the same word");
+            }
+
+        }
+
+        
+
+            
+     }
+}
+
+
+
+
+    
