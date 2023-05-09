96fdf951d7f2cba82d8afc7b1e3174e94f1d26c1
janishap <72866270+janishap@users.noreply.github.com>
2023-05-09 09:19:45 +0530

    Add files via upload

---
+++ day13/stream writer and stream reader/stream writer and stream reader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stream_writer_and_stream_reader
+{
+    internal class Program
+    {
+        static string file = "D:\\c#\\day13\\stream writer and stream reader";
+        static void Main(string[] args)
+        {
+            Write();
+            Read();
+
+        }
+        static void write()
+        {
+            StreamWriter fileStream= new StreamWriter(file);    
+            fileStream.WriteLine("what is this life is")
+        }
+    }
+}
