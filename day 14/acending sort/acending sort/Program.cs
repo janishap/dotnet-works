ba0e33d037f3e82882b6b7eca412d0241593fde7
janishap <72866270+janishap@users.noreply.github.com>
2023-05-09 09:22:52 +0530

    Add files via upload

---
+++ day 14/acending sort/acending sort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acending_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[] arr = new int[n];
+            int temp = 0;
+            Console.WriteLine("Enter the array");
+                for(int i=0;i<arr.Length;i++)
+                {
+                arr[i]=int.Parse(Console.ReadLine());
+                }
+
+               for(int j=0;j<arr.Length;j++)
+               {
+                  for (int k = j+1; k < arr.Length; k++)
+
+                  {
+                    if (arr[k] < arr[j])
+
+                    {
+                        temp = arr[k];
+                        arr[k] = arr[j];
+                        arr[j] = temp;
+                        
+                    }                
+                  }
+                
+               }
+
+            Console.WriteLine("acending  order is ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
---
+++ day 14/delegate prgrm/delegate prgrm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_prgrm
+{
+
+    
+
+    internal class Program
+    {
+
+        public delegate int refmothe(int i, int j);
+
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
