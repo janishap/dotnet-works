c375e7d390bdecdebc54d8f2960d75fdc3c3bd1e
janishap <72866270+janishap@users.noreply.github.com>
2023-05-17 09:20:39 +0530

    Add files via upload

---
+++ day 21/CarNS/CarTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+using CarNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace CarTests
+{
+    [TestClass]
+    public class CarTestsClass
+    {
+        Car test_car;
+
+        [TestInitialize]
+        public void CreateCarObject()
+        {
+            test_car = new Car("Toyota", "Prius", 10, 50);
+        }
+
+        //TODO: constructor sets gasTankLevel properly
+        public CarTestsClass(double gastanklevel)
+        {
+
+            Car cr = new Car();
+
+            cr.GasTankLevel = gastanklevel;
+
+
+        }
+
+
+        //TODO: gasTankLevel is accurate after driving within tank range
+
+        //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
+        //TODO: can't have more gas than tank size, expect an exception
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGasOverfillException()
+        {
+            test_car.AddGas(5);
+            Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+        }
+    }
+}
