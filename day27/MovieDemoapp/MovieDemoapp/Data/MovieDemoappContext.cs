c97602ef1ee66f9fae1beaa1d1f50380e40cb466
janishap <72866270+janishap@users.noreply.github.com>
2023-05-17 09:49:58 +0530

    Add files via upload

---
+++ day27/MovieDemoapp/MovieDemoapp/Data/MovieDemoappContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieDemoapp.Models;
+
+namespace MovieDemoapp.Data
+{
+    public class MovieDemoappContext : DbContext
+    {
+        public MovieDemoappContext (DbContextOptions<MovieDemoappContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieDemoapp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
