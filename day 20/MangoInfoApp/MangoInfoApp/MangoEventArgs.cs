9797c538cdf6c959c0577968a70333ecff026712
janishap <72866270+janishap@users.noreply.github.com>
2023-06-08 17:33:35 +0530

    Add files via upload

---
+++ day 20/MangoInfoApp/MangoInfoApp/MangoEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoInfoApp
+{
+    
+    class MangoEventArgs : EventArgs
+    {
+        public MangoEventArgs(string type, int number)
+        {
+            MangoInfo = type;
+            Number = number;
+        }
+        public string MangoInfo { get; private set; }
+        public int Number { get; private set; }
+    }
+}
