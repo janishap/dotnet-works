ba3c5f57a045502df9b8c26c01d815b17512ab99
janishap <72866270+janishap@users.noreply.github.com>
2023-06-10 11:40:56 +0530

    Add files via upload

---
+++ day 28/code drill/code drill/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_drill
+{
+    public class Product
+    {
+
+
+        public string _supplierName;
+
+        public long _id;
+
+        public string _productName;
+    }
+}
---
+++ day 28/code drill/code drill/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_drill
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+
+            Console.WriteLine("Enter the product id");
+            p._id = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the product name");
+            p._productName = Console.ReadLine();
+            Console.WriteLine("Enter the supplier name");
+            p._supplierName = Console.ReadLine();
+
+
+            Console.WriteLine("Product Id is " + p._id);
+
+            Console.WriteLine("Product Name is " + p._productName);
+
+
+            Console.WriteLine("Supplier Name is " + p._supplierName);
+
+
+
+
+
+
+
+        }
+    }
+}
+
+      
+
+        
+
+
+
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
